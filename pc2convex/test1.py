21c54731929e941b47a40242adb3b317958c95d0
Huimin <784453793@qq.com>
2017-10-27 19:38:34 -0400

    modify test1. test2 seems impossible.

--- pc2convex/test1.py
+++ pc2convex/test1.py
@@ -16,16 +16,16 @@ from glob import glob
 from time import time
 import pickle
 
-fpaths = glob('./a3d/a3d/*a3d')
+fpaths = glob('/ext2/seg/PSRC/Data/stage1/a3d/*a3d')
 ratio = 8
 n_layers = 30
-ifplot=True
+ifplot=False
+output_dir = './outputs2'
 
-
-if os.path.exists('./outputs'):
+if os.path.exists(output_dir):
     pass
 else:
-    os.mkdir('./outputs')
+    os.mkdir(output_dir)
 
 for fpath in fpaths:
     fname = utils.get_name(fpath)
@@ -47,24 +47,31 @@ for fpath in fpaths:
     segs = [interp(x) for x in segs]
 
     tic = time()
-    # with raw data
+    with raw data
     bita_, _ = utils.ostu3d(data_)
     pts_ = utils.get_points(bita_, thresh=0)
     labels_ = segmentation.get_labels(pts_, flags, segs, ratio=ratio)
     print('labling takes {}s.'.format(time() - tic))
 
     # for vis
-    tic = time()
-    pts = utils.get_points(bita, thresh=0)
-    labels = segmentation.get_labels(pts, flags, segs, ratio=1)
+    # tic = time()
+    # pts = utils.get_points(bita, thresh=0)
+    # labels = segmentation.get_labels(pts, flags, segs, ratio=1)
+
+    pts, labels = segmentation.get_labels_from_data(data_, flags, segs, ratio=1)
+
     if ifplot:
         utils.seg_vis2d(pts, labels, bita, savefig=True, fname= fname)
-    print('visualization takes {}s'.format(time() - tic))
-    segs_ = [np.array([np.hstack([pt, labels_[j]]) for j, pt in enumerate(pts_) if labels_[j] == i]) for i in range(18)]
-    np.save('./outputs/'+fname+'.npy', segs_)
-    with open('./outputs' + fname + '.txt', 'wb') as fid:
-        pickle.dump(flags, fid)
+        print('visualization takes {}s'.format(time() - tic))
 
+    segs_ = [np.array([np.hstack([pt, data_[pt.astype(np.int16)], labels_[j], data_[pt.astype(np.int16)]]) for j, pt in enumerate(pts_) if labels_[j] == i]) for i in range(18)]
+    np.save('./outputs/'+fname+'_segs.npy', segs_)
+
+    # np.save('./outputs/'+fname+'_pts.npy', pts)
+    # np.save('./outputs/' + fname + '_labels.npy', labels)
+    # with open('./outputs/' + fname + '.txt', 'wb') as fid:
+    #     pickle.dump(flags, fid)
+    # break
 
 
 
